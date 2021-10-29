f81a53ff4990f11b9910f0ef6c514a7dc18915c0
Egor Fedyaev <georgefed@yandex-team.ru>
2021-10-29 22:52:35 +0300

    Initial Commit

---
+++ notes_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notes_app
+//
+//  Created by Egor Fedyaev on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
